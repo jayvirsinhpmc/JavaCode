653165f8585ec896595a6650090f0d8be2e16b51
jayvirsinhpmc <Jayvirsinh.Chhasatiya@pmcretail.com>
2023-09-14 15:49:35 +0530

    First Commit.

--- src/JavaFlowControl/IfElse.java
+++ src/JavaFlowControl/IfElse.java
@@ -1,5 +1,16 @@
 package JavaFlowControl;
 
 public class IfElse {
+    public static void main(String[] args) {
 
+        int age = 19;
+
+        if (age < 18) {
+            System.out.println("You are not eligible for voting");
+        } else if (age == 18) {
+            System.out.println("You will eligible next year");
+        } else {
+            System.out.println("You are eligible for voting");
+        }
+    }
 }
